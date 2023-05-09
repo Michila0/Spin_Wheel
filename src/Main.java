460567e59d948d8f2e2a2c132314aa882412578c
Michila <michila.bimsara12@gmail.com>
2023-05-09 19:39:05 +0530

    creat the frame using Java Swing

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import javax.swing.*;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception{
+        int width = 1000;
+        int height = 1000;
+
+        JFrame fram = new JFrame();
     }
 }
