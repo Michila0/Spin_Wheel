2a1ddeca65129208145f2a660aa432f0d0e24365
Michila <michila.bimsara12@gmail.com>
2023-05-23 11:03:25 +0530

    spin function will creat

--- src/Wheel.js
+++ src/Wheel.js
@@ -3,6 +3,14 @@ function shuffle(array) {
     randomIndex;
 
     while(0 !== currentIndex) {
-        randomIndex = Math.floor(Math.random() * currentIndex)
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        [array[currentIndex], array[randomIndex]] = [array[currentIndex],array[currentIndex],];
     }
+    return array;
+}
+
+function spin() {
+    
+
 }
