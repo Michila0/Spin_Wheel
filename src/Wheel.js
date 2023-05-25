0080592a7675ce1a07bcdaa1d584ba4fd7abefba
Michila <michila.bimsara12@gmail.com>
2023-05-25 23:52:04 +0530

    change the name

--- src/Wheel.js
+++ src/Wheel.js
@@ -17,8 +17,8 @@ function spin() {
     let SelectItem = "";
 
 
-    let AC = shuffle([1890, 2250, 2610]);
-    let Camera = shuffle([1850, 2210, 2570]);
+    let Michila = shuffle([1890, 2250, 2610]);
+    let Peter = shuffle([1850, 2210, 2570]);
     let a123 = shuffle([1770, 2130, 2490]);
     let PS = shuffle([1810, 2170, 2530]);
     let Headset = shuffle([1750, 2110, 2470]);
@@ -26,8 +26,8 @@ function spin() {
     let ROG = shuffle([1570, 1930, 2290]);
 
     let result = shuffle([
-        AC[0],
-        Camera[0],
+        Michila[0],
+        Peter[0],
         a123[0],
         PS[0],
         Headset[0],
@@ -35,8 +35,8 @@ function spin() {
         ROG[0]
     ]);
 
-    if(AC.includes(result[0])) SelectItem = "Air Conditioner";
-    if(Camera.includes(result[0])) SelectItem = "Camera Sport Action";
+    if(Michila.includes(result[0])) SelectItem = "Air Conditioner";
+    if(Peter.includes(result[0])) SelectItem = "Camera Sport Action";
     if(a123.includes(result[0])) SelectItem = "Michila";
     if(PS.includes(result[0])) SelectItem = "Playsation 4 Slim";
     if(Headset.includes(result[0])) SelectItem = "HEadset Gaming";
